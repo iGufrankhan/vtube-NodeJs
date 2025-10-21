8660f9369ee54f8dc279ab1e862e385c00895fd8
iGufrankhan <gufrankhankab123@gmail.com>
2025-10-21 21:28:35 +0530

    hope it better

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -43,13 +43,39 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 
 
 
-
-
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+    const { commentId } = req.params;
+    const userId = req.user._id;
 
-})
+    // Validate commentId
+    if (!commentId || !mongoose.Types.ObjectId.isValid(commentId)) {
+        throw new ApiError(400, "Invalid commentId");
+    }
+
+    // Check if like already exists
+    const existedLike = await Like.findOne({
+        comment: commentId,
+        likedBy: userId,
+    });
+
+    // If already liked, unlike it
+    if (existedLike) {
+        await Like.findByIdAndDelete(existedLike._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, "Comment unliked successfully", existedLike));
+    }
+
+    // Otherwise, create a new like
+    const newLike = await Like.create({
+        comment: commentId,
+        likedBy: userId,
+    });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Comment liked successfully", newLike));
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
